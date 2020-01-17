41764f975200e421f940fa2e49bb7b56c2e5a457
Frederick <frederickandrew@protonmail.com>
2020-01-16 20:53:05 -0600

    Adding root Xamarin files

---
+++ message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using MyMentor.Server;
+
+namespace Message
+{
+    public class Message
+    {
+        private string Sql_query { get; set; }
+        public Message()
+        {
+        }
+        public string ToJSON()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+
+}
---
+++ utilities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+// This is a class to throw all the random functions you want in that don't belong anywhere else
+public static class Utilities
+{
+    // Gets the parent of the parent  of the current directory, returned as a string
+   public static string Get_grandparent_path()
+    {
+        string path = System.IO.Directory.GetCurrentDirectory();
+        string grandparentDir = System.IO.Directory.GetParent(path).Parent.FullName;
+        return grandparentDir;
+    }
+}
